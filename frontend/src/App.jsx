003c532ca1948c175940082d252bafe8e246c95e
lakshayGMZ <lakshaygulati1898@gmail.com>
2023-04-24 14:03:34 +0530

    add boilerplate for home and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Test from './Test'
+import Homepage from './components/home/HomePage'
+import SignUp from './components/signUp/signUp'
 
 function App() {
     return (
             <BrowserRouter>
                 <Routes>
+                    <Route index element={<Homepage />} />
+                    <Route path='/signup' element={<SignUp />} />
                     <Route path='/capture' element={<Test />} />
                 </Routes>
             </BrowserRouter>
---
+++ frontend/src/components/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css"
+
+export default function Homepage() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
